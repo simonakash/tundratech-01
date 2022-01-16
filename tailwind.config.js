b43dc8ce0c231c17b468917192fb625f79f5d4b6
SAYMON AREFIN AKASH <75067011+simonakash@users.noreply.github.com>
2022-01-16 09:54:21 -0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  content: ["./index.html","./contact.html"],
+  content: ["./index.html","./about.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+      '2.5xl': '28px',
+      '4.5xl': '40px',
+      '5.5xl': '52px',
+      },
+    },
   },
   plugins: [],
 }
